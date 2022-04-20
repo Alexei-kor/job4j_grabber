28151af4593122a07747fb7b92925e0cc86b012a
Alexei_korn <aykornjushenkov@gmail.com>
2022-04-20 23:02:25 +0300

    first commit

---
+++ src/test/java/ru/job4j/AppTest.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void test1() {
+        assertFalse(App.getResult());
+    }
+
+}
