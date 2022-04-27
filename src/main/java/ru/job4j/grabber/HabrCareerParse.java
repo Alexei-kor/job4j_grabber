0c8c6c21cd295e975a41f7f0798b91d48cb93fa1
Alexei_korn <aykornjushenkov@gmail.com>
2022-04-28 00:05:15 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,25 +5,36 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.quartz.AlertRabbit;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
-    public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
+    private static List<String> data = new ArrayList<>();
 
+    public static void main(String[] args) throws IOException {
+        Properties prop = loadProperties();
+        int pages = Integer.parseInt(prop.getProperty("countPages"));
+        for (int i = 1; i <= pages; i++) {
+            String link = String.format("%s?page=%s", PAGE_LINK, i);
+            Connection connection = Jsoup.connect(link);
+            readPage(connection.get());
+        }
+        data.forEach(System.out::println);
+    }
+
+    private static void readPage(Document document) {
+        Elements rows = document.select(".vacancy-card__inner");
         rows.forEach(row -> {
             Element titleElement = row.select(".vacancy-card__title").first();
             Element linkElement = titleElement.child(0);
@@ -35,8 +46,23 @@ public class HabrCareerParse {
             String dateStr = time.attr("dateTime");
             DateTimeParser dtParser = new HarbCareerDateTimeParser();
             LocalDateTime lDT = dtParser.parse(dateStr);
-            System.out.printf("%s %s %s%n", vacancyName, lDT.format(DateTimeFormatter.ISO_DATE), link);
+            data.add(String.format("%s %s %s",
+                    vacancyName,
+                    lDT.format(DateTimeFormatter.ISO_DATE),
+                    link)
+            );
         });
+    }
 
+    public static Properties loadProperties() {
+        Properties properties = new Properties();
+        try (InputStream inputStream = AlertRabbit.class
+                .getClassLoader()
+                .getResourceAsStream("careerParser.properties")) {
+            properties.load(inputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return properties;
     }
 }
