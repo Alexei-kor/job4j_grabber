9c0459fca32bc49c1f5782b36a31f02a8fdc8dab
Alexei_korn <aykornjushenkov@gmail.com>
2022-05-11 21:25:25 +0300

    1. Демонстрация работы GC [#1589 #298892]

---
+++ src/main/java/ru/job4j/gc/GCDemo.java
@@ -0,0 +1,36 @@
+package ru.job4j.gc;
+
+import static com.carrotsearch.sizeof.RamUsageEstimator.*;
+
+public class GCDemo {
+
+    private static final long KB = 1000;
+    private static final long MB = KB * KB;
+    private static final Runtime ENVIRONMENT = Runtime.getRuntime();
+
+    public static void info() {
+        final long freeMemory = ENVIRONMENT.freeMemory();
+        final long totalMemory = ENVIRONMENT.totalMemory();
+        final long maxMemory = ENVIRONMENT.maxMemory();
+        System.out.println("=== Environment state ===");
+        System.out.printf("Free: %d%n", freeMemory );
+        System.out.printf("Total: %d%n", totalMemory );
+        System.out.printf("Max: %d%n", maxMemory );
+    }
+
+    public static void main(String[] args) {
+        info();
+        String name = "asdfasdfasdfasdfasfgfgfgfggfdfasdfasdfasdfasdsdfghsdaserasrasdrasdtfghj"
+                + "dfsdtgsdfgsdfgsdfgdfasdfasdfsfghfghjfghjdgsfdgsdftsdtsdftsdfytsdysgfdghfjdgj"
+                + "hdfghdfghdfghasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf"
+                + "asdfasdfasdfasdfasfgfgfgfggfdfasdfasdfasdfasdsdfghsdaserasrasdrasdtfghj"
+                + "dfsdtgsdfgsdfgsdfgdfasdfasdfsfghfghjfghjdgsfdgsdftsdtsdftsdfytsdysgfdghfjdgj"
+                + "hdfghdfghdfghasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf";
+        for (int i = 0; i < 10000; i++) {
+            new Person(i, name);
+
+        }
+        info();
+    }
+
+}
